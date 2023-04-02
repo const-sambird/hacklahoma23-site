9c1d086c44692d1305dc6991005c5787801a9365
Sam Bird <sam@sambird.dev>
2023-04-02 07:36:23 -0500

    twilio

---
+++ controllers/leaderboardController.js
@@ -0,0 +1,15 @@
+const { accountSid, authToken, from } = require('../credentials.json')
+const User = require('../database/User');
+
+const leaderboardController = {};
+
+leaderboardController.defeated = to => {
+    const client = require('twilio')(accountSid, authToken);
+    client.messages.create({
+        body: 'Your score on Nguyentendo was overtaken! Can you improve your time?',
+        from: from,
+        to: to
+    })
+    .then(message => console.log(message.sid))
+    .catch(e => console.error);
+}
